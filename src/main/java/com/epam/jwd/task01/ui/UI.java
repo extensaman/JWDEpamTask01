d70ba484f131c8809c18b90422a44be8b341c5b8
extensaman <verus.wedding@gmail.com>
2022-02-19 17:59:18 +0300

    Adeed Javadoc to UI - class

--- src/main/java/com/epam/jwd/task01/ui/UI.java
+++ src/main/java/com/epam/jwd/task01/ui/UI.java
@@ -11,7 +11,7 @@ import java.util.Collection;
  */
 
 public class UI {
-    public static final double PERIODICAL_FEE_LIMIT = 30.0;
+    public static final BigDecimal PERIODICAL_FEE_LIMIT = BigDecimal.valueOf(30.0);
     public static final String ALL_TARIFFS_SAMPLE_TEXT = "All tariffs are\n";
     public static final String PERIODICAL_FEE_SORTED_SAMPLE_TEXT =
             "Tariffs that sorted by periodical fee are\n";
@@ -32,36 +32,62 @@ public class UI {
         this.clientLogic = clientLogic;
     }
 
+    /**
+     * Method for displaying the results of program execution,
+     * i.e. information required in accordance with the task
+     */
     public void start() {
         viewAllTariff();
         viewAllClient();
         viewTariffSortedByPeriodicalFee();
-        viewTariffWithInternetAndPeriodicalFeeLessThanNumber();
+        viewTariffWithInternetAndPeriodicalFeeLessThanNumber(PERIODICAL_FEE_LIMIT);
     }
 
+    /**
+     * Method to display all available mobile tariffs
+     */
     public void viewAllTariff() {
         io.output(ALL_TARIFFS_SAMPLE_TEXT);
         viewCollection(tariffLogic.getAllTariff());
     }
 
+    /**
+     * Method to display all mobile connected clients
+     */
     public void viewAllClient() {
         io.output(COUNT_OF_CLIENTS_IS + clientLogic.getClientAmount());
         io.output(ALL_CLIENTS_ARE);
         viewCollection(clientLogic.getAllClient());
     }
 
+    /**
+     * Method for displaying information about tariff plans sorted by first connection fee
+     */
     public void viewTariffSortedByPeriodicalFee() {
         io.output(PERIODICAL_FEE_SORTED_SAMPLE_TEXT);
         viewCollection(tariffLogic.getTariffSortedByPeriodicalFee());
     }
 
-    public void viewTariffWithInternetAndPeriodicalFeeLessThanNumber() {
+    /**
+     * Method for displaying information about tariff plans with Internet traffic,
+     * periodical payment less than <i>periodicalFeeHighLimit</i>
+     * and also sorted by connection fee value
+     *
+     * @param periodicalFeeHighLimit high bound for periodical fee
+     */
+    public void viewTariffWithInternetAndPeriodicalFeeLessThanNumber(BigDecimal periodicalFeeHighLimit) {
         io.output(CUSTOM_SAMPLE_TEXT);
         viewCollection(
-                tariffLogic.getTariffWithInternetAndPeriodicalFeeLessThanNumber(
-                        BigDecimal.valueOf(PERIODICAL_FEE_LIMIT)));
+                tariffLogic.getTariffWithInternetAndPeriodicalFeeLessThanNumber(periodicalFeeHighLimit));
     }
 
+    /**
+     * Method for displaying the elements of the collection.
+     * After displaying the last element
+     * in addition, a newline character is displayed
+     *
+     * @param collection collection for displaying
+     */
     private void viewCollection(Collection<?> collection) {
         for (Object o : collection) {
             io.output(o.toString() + TO_NEW_LINE);
